0199c40bfa83992ff856135628ce4f9b8f8d9360
Hoakoru <103974918+Hoakoru@users.noreply.github.com>
2023-04-21 08:47:52 -0400

    Actualizar main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,HashRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-vite-page/">
+    <HashRouter basename="/react-vite-page/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
